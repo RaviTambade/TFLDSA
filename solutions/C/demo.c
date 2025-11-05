3bbe7b91bdd03215098a2a7b5547e012244cc068
Ravi Tambade <ravi.tambade@transflower.in>
2025-11-05 23:02:29 +0530

    Thread, Stack Exeuction

---
+++ solutions/C/demo.c
@@ -0,0 +1,25 @@
+//macro : Preprocessor
+#include <stdio.h>
+
+//global variable
+int marks=56;
+
+//global function
+//Primary Thread
+//you have implementation of main
+
+
+void showDetails(){
+    printf("Transfower TAP Details !");
+
+}
+
+int main(){
+    //logic
+    //set of instructions
+    //printf function have been nested inside main
+    for(int i=0;i<=10000;i++){
+        printf("Hello world !");   //invoke
+    }
+    
+}
