7d08113d3b80b1ed93dd62bc268c0a8c5c782c0e
Ravi Tambade <ravi.tambade@transflower.in>
2025-11-07 07:56:34 +0530

    Heap memory

--- solutions/C/demo.c
+++ solutions/C/demo.c
@@ -1,33 +1,18 @@
-//macro : Preprocessor
 #include <stdio.h>
 #include <stdbool.h>
-
-
-
 //global variable
 int marks=56;
-
 void showDetails(){
-
     int sum=67;
     int * ptrResult= (int *)malloc (sizeof(int));  //pointer
     *ptrResult=89;
     printf("Transfower TAP Details !");
-
     printf( "Sum,= %d", sum);
     printf("Result= %d", *ptrResult);
-
-
 }
 
-//Blocking Call
-
 int main(){
-    //logic
-    //set of instructions
-    //printf function have been nested inside main
     for(int i=0;i<=10000;i++){
         showDetails();  //invoke
-    }
-    
+    }  
 }
